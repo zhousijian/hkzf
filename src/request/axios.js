31f23dabdcc9729e4c9fc7433d4da0bccb46699e
zhousijian <13553593242@163.com>
2020-03-22 00:06:23 +0800

    添加axios拦截器，在拦截器里添加Toast请提示

--- src/request/axios.js
+++ src/request/axios.js
@@ -1,7 +1,28 @@
 import axios from 'axios'
+import { Toast } from 'antd-mobile';
 
 export const baseURL = 'http://157.122.54.189:9060'
 
 axios.defaults.baseURL = 'http://157.122.54.189:9060'
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    Toast.loading('加载中...', 0)
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    Toast.hide()
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
 
 export default axios
