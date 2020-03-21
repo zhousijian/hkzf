ab964379680cce619fdeb84eb97c59e07994a421
zhousijian <13553593242@163.com>
2020-03-21 15:09:31 +0800

    完成租房小组的静态结构和数据渲染

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Carousel } from 'antd-mobile';
-import axios,{baseURL} from '../../request/axios'
+import axios, { baseURL } from '../../request/axios'
 import index from './index.module.scss'
 import nav1 from '../../assets/images/nav-1.png'
 import nav2 from '../../assets/images/nav-2.png'
@@ -11,16 +11,26 @@ class Index extends Component {
         super(props);
         this.state = {
             data: [],
+            groupsImg: []
         }
     }
     componentDidMount() {
         axios({
-            url : '/home/swiper'
-        }).then(res=>{
+            url: '/home/swiper'
+        }).then(res => {
             // console.log(res);
-            const {body} = res.data
+            const { body } = res.data
             this.setState({
-                data : body
+                data: body
+            })
+        })
+        axios({
+            url: '/home/groups'
+        }).then(res => {
+            // console.log(res);
+            const { body } = res.data
+            this.setState({
+                groupsImg: body
             })
         })
     }
@@ -34,32 +44,53 @@ class Index extends Component {
                         infinite
                     >
                         {this.state.data.map(val => (
-                                <img
-                                    key={val.id}
-                                    src={baseURL+val.imgSrc}
-                                    alt=""
-                                    style={{ width: '100%', verticalAlign: 'top' }}
-                                />
+                            <img
+                                key={val.id}
+                                src={baseURL + val.imgSrc}
+                                alt=""
+                                style={{ width: '100%', verticalAlign: 'top' }}
+                            />
                         ))}
                     </Carousel>}
                 </div>
                 {/* 租聘方式和找房 */}
                 <div className={index.mode}>
                     <div>
-                        <img src={nav1} alt=""/>
+                        <img src={nav1} alt="" />
                         <span>整租</span>
                     </div>
                     <div>
-                        <img src={nav2} alt=""/>
+                        <img src={nav2} alt="" />
                         <span>合租</span>
                     </div>
                     <div>
-                        <img src={nav3} alt=""/>
+                        <img src={nav3} alt="" />
                         <span>地图找房</span>
                     </div>
                     <div>
-                        <img src={nav4} alt=""/>
+                        <img src={nav4} alt="" />
                         <span>去出租</span>
+                    </div>
+                </div>
+                {/* 租房小组 */}
+                <div className={index.groups}>
+                    <div className={index.title}>
+                        <h3>租房小组</h3>
+                        <span>更多</span>
+                    </div>
+                    <div className={index.content}>
+                        {this.state.groupsImg.map(v => {
+                            return <div className={index.content_item} key={v.id}>
+                                <div className={index.content_item_left}>
+                                    <strong>{v.title}</strong>
+                                    <p></p>
+                                    <span>{v.desc}</span>
+                                </div>
+                                <div className={index.content_item_right}>
+                                    <img src={baseURL+v.imgSrc} alt="" />
+                                </div>
+                            </div>
+                        })}
                     </div>
                 </div>
             </div>
