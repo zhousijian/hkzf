41d3f911c5513c0291c1c2d3f45c7330445ced53
zhousijian <13553593242@163.com>
2020-03-23 00:00:59 +0800

    完成城市列表页的顶部NavBar

--- src/pages/cityList/index.js
+++ src/pages/cityList/index.js
@@ -1,17 +1,24 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
 
 class CityList extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
-                CityList城市列表页
+                <div className="navBar">
+                    <NavBar
+                        mode="light"
+                        icon={<Icon type="left" />}
+                        onLeftClick={() => this.props.history.go(-1)}
+                    >城市选择</NavBar>
+                </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default CityList;
