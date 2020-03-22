5aea8f4be76f87b32d086c45c7b614a1272c3b47
zhousijian <13553593242@163.com>
2020-03-23 00:09:15 +0800

    使用插件可以获取数据的store仓库代码

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
+import { createStore, applyMiddleware,compose } from 'redux';
 import reducer from "./reducer";
-// 1 引 入  thunk 用来实现异步action
-import thunk from "redux-thunk";
-// 2 引入 redux的中间件 加载器 
-import {createStore,applyMiddleware  } from "redux";
-export default createStore(reducer,applyMiddleware(thunk));
+import thunk from 'redux-thunk'; 
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
+const store = createStore(reducer, enhancer);
+export default  store;
