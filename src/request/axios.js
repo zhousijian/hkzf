02f348a267b3e6c2bb69ade6477e67f5071a573e
zhousijian <13553593242@163.com>
2020-03-22 16:37:45 +0800

    提示体验感，在拦截器里添加计数器

--- src/request/axios.js
+++ src/request/axios.js
@@ -5,9 +5,12 @@ export const baseURL = 'http://157.122.54.189:9060'
 
 axios.defaults.baseURL = 'http://157.122.54.189:9060'
 
+// 计数器
+let AjaxTimes = 0
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
+    AjaxTimes++
     Toast.loading('加载中...', 0)
     return config;
   }, function (error) {
@@ -18,7 +21,11 @@ axios.interceptors.request.use(function (config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
-    Toast.hide()
+    AjaxTimes--
+    if(AjaxTimes === 0){
+      // 等最后一个请求回来了才把轻提示隐藏
+      Toast.hide()
+    }
     return response;
   }, function (error) {
     // 对响应错误做点什么
