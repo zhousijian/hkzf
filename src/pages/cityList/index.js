bf09806b53ca695518bf42a11bf005aa8c66f653
zhousijian <13553593242@163.com>
2020-03-24 20:37:31 +0800

    完成可视区域渲染插件的引入和样式调整

--- src/pages/cityList/index.js
+++ src/pages/cityList/index.js
@@ -3,10 +3,11 @@ import { NavBar, Icon } from 'antd-mobile';
 import { connect } from "react-redux";
 import axios from '../../request/axios';
 import cityListScss from "./index.module.scss";
+import {List} from 'react-virtualized';
 
 class CityList extends Component {
     state = {
-        list: []
+        list: [],
     }
     async componentDidMount() {
         let allCityData = []
@@ -53,6 +54,25 @@ class CityList extends Component {
         })
 
     }
+    // 循环体
+    rowRenderer = ({ key, index, isScrolling, isVisible, style, }) => {
+        return (
+            // <div key={key} style={style}>
+            //     {this.state.list[index].name}
+            // </div>
+            <div className={cityListScss.types} key={key} style={style}>
+            <div className={cityListScss.text}>{this.state.list[index].name}</div>
+            {this.state.list[index].values.map((vv, ii) => <div className={cityListScss.city} key={ii}>
+                {vv.name}
+            </div>)}
+        </div>
+        );
+    }
+
+    // 行高度
+    rowHeight = ({index})=>{
+        return 40 + this.state.list[index].values.length * 40
+    }
 
     render() {
         return (
@@ -66,14 +86,21 @@ class CityList extends Component {
                     >城市选择</NavBar>
                 </div>
                 {/* 城市列表 */}
-                {this.props.initCity && <div className={cityListScss.cityList}>
-                    {this.state.list.map((v, i) => <div className={cityListScss.types} key={i}>
+                <div className={cityListScss.cityList}>
+                    {/* {this.state.list.map((v, i) => <div className={cityListScss.types} key={i}>
                         <div className={cityListScss.text}>{v.name}</div>
                         {v.values.map((vv, ii) => <div className={cityListScss.city} key={ii}>
                             {vv.name}
                         </div>)}
-                    </div>)}
-                </div>}
+                    </div>)} */}
+                    <List
+                        width={window.screen.width}
+                        height={window.screen.height - 45}
+                        rowCount={this.state.list.length}
+                        rowHeight={this.rowHeight}
+                        rowRenderer={this.rowRenderer}
+                    />
+                </div>
             </div>
         );
     }
