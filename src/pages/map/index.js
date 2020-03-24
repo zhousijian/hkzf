50350652a2d715f3e9b52166d02eaccfd63ada46
zhousijian <13553593242@163.com>
2020-03-25 00:54:11 +0800

    完成地图找房页面的地图引入缩放和比例尺控件

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,17 +1,42 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
+import mapScss from "./index.module.scss";
+import { connect } from "react-redux";
 
 class Map extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+    componentDidMount() {
+        // 创建地图实例  
+        var map = new window.BMap.Map("container");
+        // 创建点坐标  
+        var point = new window.BMap.Point(116.404, 39.915);
+        // 初始化地图，设置中心点坐标和地图级别 
+        map.centerAndZoom(point, 15);
+        // 填写控件-平移缩放控件
+        map.addControl(new window.BMap.NavigationControl()); 
+        map.addControl(new window.BMap.ScaleControl()); 
+        map.addControl(new window.BMap.GeolocationControl());      
     }
-    render() { 
-        return ( 
-            <div>
-                Map地图页
+    render() {
+        return (
+            <div className={mapScss.map}>
+                {/* 头部 */}
+                <div className={mapScss.header_navBar}>
+                    <NavBar
+                        mode="light"
+                        icon={<Icon type="left" />}
+                        onLeftClick={() => this.props.history.go(-1)}
+                    >地图找房</NavBar>
+                </div>
+                <div id='container' className={mapScss.container}></div>
             </div>
-         );
+        );
     }
 }
- 
-export default Map;
+
+const mapStateToProps = (state) => {
+    return {
+      city: state.mapReducer.cityLocation.name
+    }  
+  }
+
+export default connect(mapStateToProps)(Map);
