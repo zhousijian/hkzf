0c07ddf5a2da85d3c6eaba853587fa1b6aa6b482
zhousijian <13553593242@163.com>
2020-03-27 16:42:48 +0800

    完成找房页面的头部部分

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,17 +1,32 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
+import CityInput from '../../components/cityInput'
+import indexCss from "./index.module.scss";
 
 class List extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div>
-                list列表页
+    render() {
+        return (
+            <div className={indexCss.found_list}>
+                {/* 头部 */}
+                <div className={indexCss.header}>
+                    <NavBar
+                        style={{backgroundColor:'#ddd'}}
+                        mode="light"
+                        icon={<Icon type="left" />}
+                        onLeftClick={() => this.props.history.go(-1)}
+                    >NavBar</NavBar>
+                    <div className={indexCss.city_input}>
+                        <CityInput></CityInput>
+                    </div>
+                </div>
+                {/*  */}
             </div>
-         );
+        );
     }
 }
- 
+
 export default List;
