97fd0fb65d2f524acefed0dd388b2aafcecd164d
zhousijian <13553593242@163.com>
2020-03-28 00:56:21 +0800

    测试组件的动画效果

--- src/components/sliderBar/index.js
+++ src/components/sliderBar/index.js
@@ -2,15 +2,15 @@ import React,{ Component } from "react";
 import indexCss from "./index.module.scss";
 
 class SliderBar extends Component {
-    static defaultProps = {
-        show : false
-    }
+    // static defaultProps = {
+    //     show : true
+    // }
     state = {  }
     render() { 
         const indexOut = indexCss.index_out
         const indexIn = indexCss.index_in
         return ( 
-            <div className={[indexCss.index,indexOut,this.props.show ? indexIn:''].join(' ')}>
+            <div className={[indexCss.index,indexOut,indexIn].join(' ')}>
                 {this.props.children}
             </div>
          );
